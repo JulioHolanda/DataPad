51a02216da5e687b9565ad8bb421101927cb137e
gl-schneck <99268789+gl-schneck@users.noreply.github.com>
2023-05-22 17:12:11 -0300

    Adicionar classe Contato

---
+++ DataPad/src/br/gov/school/projetos3/gerenciamento/entidades/Contato.java
@@ -0,0 +1,22 @@
+package DataPad.src.br.gov.school.projetos3.gerenciamento.entidades;
+
+public class Contato {
+	private long id;
+	private String nome;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
