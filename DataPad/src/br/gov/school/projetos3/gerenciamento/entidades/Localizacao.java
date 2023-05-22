e966dd2b5984ea24b6610319183e7e94cdb25697
dudumcm <emcm@cesar.school>
2023-05-21 23:11:27 -0300

    Adição da classe de localização

---
+++ DataPad/src/br/gov/school/projetos3/gerenciamento/entidades/Localizacao.java
@@ -0,0 +1,41 @@
+package br.gov.school.projetos3.gerenciamento.entidades;
+
+public class Localizacao {
+    private int numero;
+    private String complemento;
+    private String rua;
+    private String estadoProv;
+    private String pais;
+
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getEstadoProv() {
+        return estadoProv;
+    }
+    public void setEstadoProv(String estadoProv) {
+        this.estadoProv = estadoProv;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+}
