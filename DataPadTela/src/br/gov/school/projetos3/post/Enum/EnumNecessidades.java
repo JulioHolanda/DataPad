817c6fe4e64ced9c49b14ab052dd545e7809913b
Julio H Cavalcanti <juliophc@msn.com>
2023-06-10 22:35:56 -0300

    enumNecessidades

--- DataPadTela/src/br/gov/school/projetos3/post/Enum/EnumNecessidades.java
+++ DataPadTela/src/br/gov/school/projetos3/post/Enum/EnumNecessidades.java
@@ -1,12 +1,39 @@
 package br.gov.school.projetos3.post.Enum;
 
-public enum EnumNecessidades {
-	HIGIENE,
-	ALIMENTO,
-	METERIAL_DE_CONSTRUCAO,
-	MEDICAMENTO,
-	VESTIMENTAS,
-	MATERIAL_ESCOLAR,
-	MARERIAIS_ASSISTIVOS,
-	MATERIAL_DE_LIMPEZA
+import java.io.Serializable;
+
+import br.gov.school.projetos3.util.Enum.EnumCategoria;
+
+public enum EnumNecessidades implements Serializable{
+	HIGIENE(1, "Higiene"),
+	ALIMENTO(2, "Alimento"),
+	MATERIAL_DE_CONSTRUCAO(3, "Material de Construção"),
+	MEDICAMENTO(4, "Medicamento"),
+	VESTIMENTAS(5, "Vestimentas"),
+	MATERIAL_ESCOLAR(6, "Material Escolar"),
+	MATERIAIS_ASSISTIVOS(7, "Materiais Assistivos"),
+	MATERIAL_DE_LIMPEZA(8, "Material de Limpeza");
+    
+    private int codigo;
+    private String descricao;
+    
+    private EnumNecessidades(int codigo, String descricao) {
+        this.codigo=codigo;
+        this.descricao=descricao;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public static EnumNecessidades obterPorCodigo(int codigo) {
+        for(EnumNecessidades necessidade : EnumNecessidades.values()) {
+            if(necessidade.getCodigo()==codigo) {
+                return necessidade;
+            }
+        }
+        return null;
+    }
 }
