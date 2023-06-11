dd5f0256a9625e3871a6ddf62de46e01988170fb
Julio H Cavalcanti <juliophc@msn.com>
2023-06-10 22:30:27 -0300

    menu inicial

--- DataPadTela/src/BussolaSolidaria.java
+++ DataPadTela/src/BussolaSolidaria.java
@@ -14,157 +14,195 @@ public class BussolaSolidaria {
         
     	String temp;
     	
-        System.out.println("==== Cadastro de Usuário ====");
-        
-        do {
-        System.out.print("Nome: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String nome = ENTRADA.nextLine();
-        temp = "";
-        
-        do {
-        System.out.print("Senha: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String senha = ENTRADA.nextLine();
-        temp = "";
-        
-        System.out.println("\n==== Cadastro de Projeto Social ====");
-        
-        do {
-        System.out.print("Nome Fantasia: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String nomeFantasia = ENTRADA.nextLine();
-        temp = "";
+    	int opcao = -1;
+    	while (opcao != 0) {
 
-        do {
-        System.out.print("CPF/CNPJ: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String cadastroFisJur = ENTRADA.nextLine();
-        temp = "";
+	        System.out.println("Bem-vindo(a) à Tela Inicial");
+	        System.out.println("============================");
+	        System.out.println("Selecione uma opção:");
+	        System.out.println("1. Visualizar Projetos");
+	        System.out.println("2. Login");
+	        System.out.println("3. Cadastrar");
+	        System.out.println("0. Sair");
 
-        EnumCategoria[] enumCategoria = EnumCategoria.values();
-        int cont = 1;
-        do {
-        System.out.print("Categoria (escolha por número): \n");
-        for (EnumCategoria eCategoria:enumCategoria ) {
-        	System.out.println(cont+ " - " + eCategoria+"\n");
-        	cont++;
+            System.out.print("Digite o número da opção desejada: ");
+            opcao = ENTRADA.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Opção selecionada: Visualizar Projetos");
+                    // Chamar método para realizar a busca
+                    break;
+                case 2:
+                    System.out.println("Opção selecionada: Login");
+                    // Chamar método para realizar o login
+                    break;
+                case 3:
+                    System.out.println("Opção selecionada: Cadastrar\n");
+                    
+                    System.out.println("==== Cadastro de Usuário ====\n");
+                    
+                    do {
+                    System.out.print("Nome: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String nome = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    do {
+                    System.out.print("Senha: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String senha = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    System.out.println("\n==== Cadastro de Projeto Social ====\n");
+                    
+                    do {
+                    System.out.print("Nome Fantasia: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String nomeFantasia = ENTRADA.nextLine();
+                    temp = "";
+
+                    do {
+                    System.out.print("CPF/CNPJ: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String cadastroFisJur = ENTRADA.nextLine();
+                    temp = "";
+
+                    EnumCategoria[] enumCategoria = EnumCategoria.values();
+                    int cont = 1;
+                    do {
+                    System.out.print("Categoria (escolha por número): \n");
+                    for (EnumCategoria eCategoria:enumCategoria ) {
+                    	System.out.println(cont+ " - " + eCategoria+"\n");
+                    	cont++;
+                    }
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null || Integer.parseInt(temp)<1 || Integer.parseInt(temp)>6 );
+                    String areaAtacao = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    EnumNicho[] enumNicho = EnumNicho.values();
+                    cont = 1;
+                    do {
+                    System.out.print("Publico Alvo (escolha por número): \n");
+                    for(EnumNicho eNicho: enumNicho) {
+                    	System.out.println(cont+ " - " + eNicho+"\n");
+                    	cont++;
+                    }
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null ||Integer.parseInt(temp)<1 || Integer.parseInt(temp)>11);
+                    String nicho = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    Categoria categoria = new Categoria();
+                    categoria.setCategoria(EnumCategoria.obterPorCodigo(Integer.parseInt(areaAtacao)));
+                    categoria.setNicho(EnumNicho.obterPorCodigo(Integer.parseInt(nicho)));
+
+                    do {
+                    System.out.print("(Descricao) Fale um pouco sobre o projeto: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String descricao = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    System.out.println("\n==== Cadastro de Localização ====\n");
+                    
+                    do {
+                    System.out.print("País: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String pais = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    do {
+                    System.out.print("Estado/Província: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String estado = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    do {
+                    System.out.print("Rua: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String rua = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    do {
+                    System.out.print("Número do Imóvel: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String numEndereco = ENTRADA.nextLine();
+                    temp = "";
+                    
+
+                    System.out.print("Complemento (não obrigatório): ");
+                    temp = ENTRADA.nextLine();
+                    String complemento = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    Localizacao localUProjetoSocial = new Localizacao();
+                    localUProjetoSocial.setComplemento(complemento);
+                    localUProjetoSocial.setEstadoProv(estado);
+                    localUProjetoSocial.setNumero(numEndereco);
+                    localUProjetoSocial.setPais(pais);
+                    localUProjetoSocial.setRua(rua);
+                    
+                    System.out.println("\n==== Cadastro de Contato ====\n");
+                    
+                    do {
+                    System.out.print("Telefone: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String telefone = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    do {
+                    System.out.print("Email: ");
+                    temp = ENTRADA.nextLine();
+                    }while(temp == "" || temp == null);
+                    String email = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    System.out.print("Rede social 1 (não obrigatório): ");
+                    temp = ENTRADA.nextLine();
+
+                    String redeSocial1 = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    System.out.print("Rede social 2 (não obrigatório): ");
+                    temp = ENTRADA.nextLine();
+                    String redeSocial2 = ENTRADA.nextLine();
+                    temp = "";
+                    
+                    Contato contatoNovoProjeto = new Contato();
+                    contatoNovoProjeto.setEmail(email);
+                    contatoNovoProjeto.setRedeSocial1(redeSocial1);
+                    contatoNovoProjeto.setRedeSocial2(redeSocial2);
+                    contatoNovoProjeto.setTelefone(telefone);
+             
+                    UProjetoSocialMediator projetoMediator = UProjetoSocialMediator.getInstance();
+                    
+                    UProjetoSocial novoProjeto = new UProjetoSocial(projetoMediator.gerarIdDeCnpj(cadastroFisJur), senha, nome, localUProjetoSocial, nomeFantasia, cadastroFisJur,
+                    		categoria, contatoNovoProjeto, descricao);
+                    
+                    System.out.println(projetoMediator.incluir(novoProjeto));
+                    // Chamar método para realizar o cadastro
+                    break;
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Digite um número válido.");
+                    break;
+            }
         }
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null || Integer.parseInt(temp)<1 || Integer.parseInt(temp)>6 );
-        String areaAtacao = ENTRADA.nextLine();
-        temp = "";
+    	
         
-        EnumNicho[] enumNicho = EnumNicho.values();
-        cont = 1;
-        do {
-        System.out.print("Publico Alvo (escolha por número): \n");
-        for(EnumNicho eNicho: enumNicho) {
-        	System.out.println(cont+ " - " + eNicho+"\n");
-        	cont++;
-        }
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null ||Integer.parseInt(temp)<1 || Integer.parseInt(temp)>11);
-        String nicho = ENTRADA.nextLine();
-        temp = "";
-        
-        Categoria categoria = new Categoria();
-        categoria.setCategoria(EnumCategoria.obterPorCodigo(Integer.parseInt(areaAtacao)));
-        categoria.setNicho(EnumNicho.obterPorCodigo(Integer.parseInt(nicho)));
-
-        do {
-        System.out.print("(Descricao) Fale um pouco sobre o projeto: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String descricao = ENTRADA.nextLine();
-        temp = "";
-        
-        System.out.println("\n==== Cadastro de Localização ====");
-        
-        do {
-        System.out.print("País: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String pais = ENTRADA.nextLine();
-        temp = "";
-        
-        do {
-        System.out.print("Estado/Província: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String estado = ENTRADA.nextLine();
-        temp = "";
-        
-        do {
-        System.out.print("Rua: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String rua = ENTRADA.nextLine();
-        temp = "";
-        
-        do {
-        System.out.print("Número do Imóvel: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String numEndereco = ENTRADA.nextLine();
-        temp = "";
-        
-
-        System.out.print("Complemento (não obrigatório): ");
-        temp = ENTRADA.nextLine();
-        String complemento = ENTRADA.nextLine();
-        temp = "";
-        
-        Localizacao localUProjetoSocial = new Localizacao();
-        localUProjetoSocial.setComplemento(complemento);
-        localUProjetoSocial.setEstadoProv(estado);
-        localUProjetoSocial.setNumero(numEndereco);
-        localUProjetoSocial.setPais(pais);
-        localUProjetoSocial.setRua(rua);
-        
-        System.out.println("\n==== Cadastro de Contato ====");
-        
-        do {
-        System.out.print("Telefone: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String telefone = ENTRADA.nextLine();
-        temp = "";
-        
-        do {
-        System.out.print("Email: ");
-        temp = ENTRADA.nextLine();
-        }while(temp == "" || temp == null);
-        String email = ENTRADA.nextLine();
-        temp = "";
-        
-        System.out.print("Rede social 1 (não obrigatório): ");
-        temp = ENTRADA.nextLine();
-
-        String redeSocial1 = ENTRADA.nextLine();
-        temp = "";
-        
-        System.out.print("Rede social 2 (não obrigatório): ");
-        temp = ENTRADA.nextLine();
-        String redeSocial2 = ENTRADA.nextLine();
-        temp = "";
-        
-        Contato contatoNovoProjeto = new Contato();
-        contatoNovoProjeto.setEmail(email);
-        contatoNovoProjeto.setRedeSocial1(redeSocial1);
-        contatoNovoProjeto.setRedeSocial2(redeSocial2);
-        contatoNovoProjeto.setTelefone(telefone);
- 
-        UProjetoSocialMediator projetoMediator = UProjetoSocialMediator.getInstance();
-        
-        UProjetoSocial novoProjeto = new UProjetoSocial(projetoMediator.gerarIdDeCnpj(cadastroFisJur), senha, nome, localUProjetoSocial, nomeFantasia, cadastroFisJur,
-        		categoria, contatoNovoProjeto, descricao);
-        
-        System.out.println(projetoMediator.incluir(novoProjeto));
     }
 
 	
